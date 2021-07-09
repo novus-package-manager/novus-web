3930bfd39c63d5df88b174256ec4aa1ec761d400
Varun Potti <potti.varun07@gmail.com>
2021-07-09 13:14:59 +0530

    fix: User input and animations

--- components/Landing/Card/Card.tsx
+++ components/Landing/Card/Card.tsx
@@ -1,26 +1,49 @@
-import styles from './Card.module.css'
+import styles from "./Card.module.css";
+import Fade from "react-reveal/Fade";
 
 interface CardProps {
-  title: string
-  description: string
-  imgWidth?: string
+  title: string;
+  description: string;
+  imgWidth?: string;
+  left: boolean;
 }
 
-const Card = ({ title, description, imgWidth = '37rem' }: CardProps) => {
-  return (
-    <div className={styles.card}>
-      <div className={styles.left}>
-        <h2 className={styles.title}>{title}</h2>
-        <h5 className={styles.description}>{description}</h5>
-      </div>
-      <img
-        className={styles.image}
-        src={`/images/${title}.png`}
-        style={{ width: imgWidth }}
-        alt={title}
-      />
-    </div>
-  )
-}
+const Card = ({ title, description, imgWidth = "37rem", left }: CardProps) => {
+  if (left) {
+    return (
+      <Fade right collapse>
+        <div className={styles.card}>
+          <div className={styles.left}>
+            <h2 className={styles.title}>{title}</h2>
+            <h5 className={styles.description}>{description}</h5>
+          </div>
+          <img
+            className={styles.image}
+            src={`/images/${title}.png`}
+            style={{ width: imgWidth }}
+            alt={title}
+          />
+        </div>
+      </Fade>
+    );
+  } else {
+    return (
+      <Fade left collapse>
+        <div className={styles.card}>
+          <img
+            className={styles.image}
+            src={`/images/${title}.png`}
+            style={{ width: imgWidth }}
+            alt={title}
+          />
+          <div className={styles.left}>
+            <h2 className={styles.title}>{title}</h2>
+            <h5 className={styles.description}>{description}</h5>
+          </div>
+        </div>
+      </Fade>
+    );
+  }
+};
 
-export default Card
+export default Card;
