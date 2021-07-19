148eed281dc596206eeb21083afea3042cecf33e
Shadow7258 <pranav.rdoshi@gmail.com>
2021-07-19 10:47:09 +0530

    fix: google analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,11 @@ export default function Home() {
             as='font'
             crossOrigin=''
           />
+          <script
+            async
+            src='https://www.googletagmanager.com/gtag/js?id=G-LSWBJXCR10'
+          ></script>
+          <script src='/scripts/google_analytics.js'></script>
         </Head>
         <Navbar page='home' />
         <Landing />
